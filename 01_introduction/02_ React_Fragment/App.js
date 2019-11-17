a8a3364b78de197ce0d83957ab66298f40c73263
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-16 21:18:17 -0800

    finished the introduction section

--- 01_introduction/02_ React_Fragment/App.js
+++ 01_introduction/02_ React_Fragment/App.js
@@ -2,10 +2,11 @@ import React from "react"
 import Child from "./Child"
 
 function App() {
-    return (
-        <div>
+    return ( // fragements can change the relationships of child/parents within the DOM
+             // it "flattens" the relationships
+        <>
             <Child />
-        </div>
+        </>
     )
 }
 
---
+++ 01_introduction/04_Prop Types/resources.md
@@ -0,0 +1,3 @@
+React Docs on PropTypes:
+https://reactjs.org/docs/typechecking-with-proptypes.html#proptypes
+https://blog.logrocket.com/validating-react-component-props-with-prop-types-ef14b29963fc/
