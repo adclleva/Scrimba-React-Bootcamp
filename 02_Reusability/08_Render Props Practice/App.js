8fc648343747cce77d4717b6d95f7f15ec6143bd
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-23 15:04:17 -0800

    Render Props Practice

---
+++ 02_Reusability/08_Render Props Practice/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import DataFetcher from "./DataFetcher"
+
+function App() {    
+    return (
+        <div>
+            <DataFetcher url="https://swapi.co/api/people/1">
+                {(data, loading) => {
+                    return (
+                        loading ? 
+                        <h1>Loading...</h1> :
+                        <p>{JSON.stringify(data)}</p>
+                    )
+                }}
+            </DataFetcher>
+        </div>
+    )
+}
+
+export default App
---
+++ 02_Reusability/08_Render Props Practice/README.md
@@ -0,0 +1,16 @@
+# Render Props practice
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
