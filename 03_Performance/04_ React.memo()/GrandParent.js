25196cc4539ff6d7bd4b8d62e1bbd583177f13f2
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-23 17:07:50 -0800

    Utilized and learned about React.memo for optimization and to prevent rendering unessessary components for functional components

---
+++ 03_Performance/04_ React.memo()/GrandParent.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Parent from "./Parent"
+
+
+// no need to premptively optimize as it costs lost of time/money for the company
+function GrandParent(props) {    
+    console.log("[👴🏼]   [ ]   [ ]   [ ] rendered")
+    return (
+        <div>
+            <p>I'm a GrandParent Component</p>
+            <Parent />
+            <Parent />
+        </div>
+    )
+}
+
+function areEqual(prevProps, nextProps) {
+  /*
+  return true if passing nextProps to render would return
+  the same result as passing prevProps to render,
+  otherwise return false
+  */
+}
+
+export default React.memo(GrandParent, areEqual)
---
+++ 03_Performance/04_ React.memo()/README.md
@@ -0,0 +1,16 @@
+# React.memo()
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
