3268380dc697b9212642d8bac9ba00bc2a92a289
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-29 15:35:38 -0800

    Started learning more of React Router and getting it set up and learning that the BrowserRouter isa context provider

---
+++ 06_React Router/01_ React Router - BrowserRouter/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom" // BrowserRouter is a context provider
+
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
